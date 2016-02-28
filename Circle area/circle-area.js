3f4137b8411d60c83d6dbb7425bc8e1f8dba52e3
ashikh <ashee.vajra@gmail.com>
2016-02-28 09:31:12 +0530

    Current time assignment done

--- Circle area/circle-area.js
+++
@@ -1,9 +0,0 @@
-function calcCircleArea(r) {
-    var Area;
-    Area = Math.PI * r * r;
-    document.writeln('r = ' + r + ';  ' + 'Area = ' + Area + '<br>'); 
-}
-var radius;
-radius=7; calcCircleArea(radius);
-radius=1.5; calcCircleArea(radius);
-radius=20; calcCircleArea(radius);
