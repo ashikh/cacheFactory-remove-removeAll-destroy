1e2d5149f46c4561e109f6f30744d59c8aa12032
ashikh <ashee.vajra@gmail.com>
2016-04-20 13:37:38 +0530

    assignment done

---
+++ Person details/person.js
@@ -0,0 +1,10 @@
+(function (name, age, city){
+    /*var person=new Object();
+    person.name=name;
+    person.age=age;
+    person.city=city;*/
+   document.writeln('Name of person:'+name);
+    document.writeln('<br>Age of person:'+age);
+    document.writeln('<br>City of person:'+city);
+})('raju', 10, 'bangalore');
+
