83d3c945ee5d0c0393e6a5fdb4783528f9deac60
ashikh <ashee.vajra@gmail.com>
2016-04-24 20:47:35 +0530

    assignment done

--- Change background/index.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-			    $("button").click(function(){
-                    var child = $("#heading").children("h1");
-			        child.addClass("bkg");
-			    });
-			});
