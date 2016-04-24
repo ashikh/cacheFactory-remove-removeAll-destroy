407fa3c3863ca92e306a10ee352efa7fb3f5dd47
ashikh <ashee.vajra@gmail.com>
2016-04-24 21:14:20 +0530

    assignment done

---
+++ blur event/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#field1").blur(function(){
+        $(this).css("background-color", "#0000ff");
+    });
+});
--- double click/dclick.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-        $("#p1").dblclick(function(){
-                $(this).append("<p>This is a double-click Event</p>");
-                });
-        $("#p1").click(function(){
-                $(this).append("<p>This is a click Event</p>");
-                }); 
-    });
