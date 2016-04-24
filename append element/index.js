24b9e1a5768f5e8be3af8acdcb5efaa8cd908027
ashikh <ashee.vajra@gmail.com>
2016-04-24 21:23:54 +0530

    assignment done

--- append element/index.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-        $("input").change(function(){
-                $("#form1").append("<p>Input field was changed</p>");
-                });
-    });
---
+++ change background colour/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        $("p").css("background-color", "#ff0000");
+    });
+});   
