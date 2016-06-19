f625958656d05feb3fc69b6c3173e6f72f06c90d
ashikh <ashee.vajra@gmail.com>
2016-06-19 17:51:32 +0530

    assignment done

--- two way binding/app.js
+++ two way binding/app.js
@@ -1,11 +1,13 @@
 var app=angular.module('myApp', []);
 		app.controller('myCtrl', function($scope){
-			$scope.msg='This is my message';
+			$scope.msg='This message is displayed in controller scope';
 		});
 		app.directive('txtDisplay', function(){
 			return {
 				restrict:'EA',
-				scope:{},
+				scope:{
+					txt:'=myDisp'
+				},
 				templateUrl:'templates/disp.html'
 			};
 		});
