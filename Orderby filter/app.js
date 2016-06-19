728f1fe1b8c803ee20014f3dbe74e0c57ef68a03
ashikh <ashee.vajra@gmail.com>
2016-06-19 16:59:43 +0530

    assignment done

--- Orderby filter/app.js
+++
@@ -1,5 +0,0 @@
-var app=angular.module('myapp', []);
-	app.controller('myctrl', function($scope)
-		{
-	$scope.session=[{name:'Ravi',topic:'HTML'}, {name:'Gaurav',topic:'CSS'}, {name:'Amit',topic:'Javascript'}, {name:'Dhanush',topic:'Bootstrap'}, {name:'Hari',topic:'AngularJS'}];
-	});
