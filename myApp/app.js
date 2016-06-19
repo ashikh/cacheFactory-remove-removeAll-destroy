90e3af5eae4c5652c564955dc47f370c0ccb7296
ashikh <ashee.vajra@gmail.com>
2016-06-19 16:29:21 +0530

    assignment done

--- myApp/app.js
+++ myApp/app.js
@@ -1,6 +1,6 @@
 var app=angular.module('myapp', []);
 	app.controller('mycontroller', function($scope)
 		{
-	$scope.name='Ravi';
-	$scope.place='Bangalore';
+	$scope.name='Ashok';
+	$scope.lastName='Kumar';
 	});
