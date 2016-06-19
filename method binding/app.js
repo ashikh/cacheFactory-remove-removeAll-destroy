4405a5568742949e3dc028b6a3661a9c1bd855c0
ashikh <ashee.vajra@gmail.com>
2016-06-19 17:59:13 +0530

    assignment done

--- method binding/app.js
+++ method binding/app.js
@@ -1,12 +1,16 @@
 var app=angular.module('myApp', []);
 		app.controller('myCtrl', function($scope){
-			$scope.msg='This message is displayed in controller scope';
+			$scope.msg='This message is displayed using parent scope';
+			$scope.alrtMsg=function(){
+			alert('This message is displayed using method binding in isolated scope');	
+			};
 		});
 		app.directive('txtDisplay', function(){
 			return {
 				restrict:'EA',
 				scope:{
-					txt:'@myDisp'
+					txt:'=myDisp',
+					alrtMsg:'&dispMethod'
 				},
 				templateUrl:'templates/disp.html'
 			};
